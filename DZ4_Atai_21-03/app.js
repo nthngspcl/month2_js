39a5f4e519f2bc3d3769774ef00afc6a02f706fd
nthngspcl <91619268+nthngspcl@users.noreply.github.com>
2022-12-16 22:24:22 +0600

    Update app.js

--- DZ4_Atai_21-03/app.js
+++ DZ4_Atai_21-03/app.js
@@ -1,43 +1,35 @@
-var tags = ["li","span","div","h3","a","div","span","li","div","div"];
-var count = {
-    li:null,
-    span:null,
-    div:null,
-    a:null,
-    h3:null
+var tags = ["li","span","div","h3","a","div","span","li","div","div","div"];
+var tags_uniq = Array.from(new Set(tags));
+var arrToObj = [];
+for (var x = 0;x<=tags_uniq.length-1;x++){
+    arrToObj[x] = [tags_uniq[x],null]
 }
-for(var i of tags){
-    switch (i){
-        case "li":
-            count.li++;
-            break;
-        case "div":
-            count.div++;
-            break;
-        case "span":
-            count.span++;
-            break;
-        case "a":
-            count.a++;
-            break;
-        case "h3":
-            count.h3++;
-            break;
+var count = Object.fromEntries(arrToObj);
+
+//считает повторные
+for(var key in count){
+    for(var tag of tags){
+        if(key === tag){
+            count[key]++    
+        }
     }
 }
 console.log(count);
-var arr1 = Object.entries(count);
-bubbleSortConcept1(arr1);
-var counted = Object.fromEntries(arr1);
+
+//сортировка
+var objToArr = Object.entries(count);
+bubbleSortConcept1(objToArr);
+console.log(objToArr);
+var counted = Object.fromEntries(objToArr);
 console.log(counted)
 
 function bubbleSortConcept1(arr) {
     for (let j = arr.length - 1; j > 0; j--) {
       for (let i = 0; i < j; i++) {
         if (arr[i][1] < arr[i + 1][1]) {
-          let temp = arr[i][1];
-          arr[i][1] = arr[i + 1][1];
-          arr[i + 1][1] = temp;
+          let temp = arr[i];
+          arr[i] = arr[i + 1];
+          arr[i + 1] = temp;
         }
       }
     }
